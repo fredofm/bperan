282ecb187b8c2131998329701def1a9f1a4eaa96
fredofm <alfredo.fm@gmail.com>
2016-04-16 19:49:16 +0200

    Creación estructura

--- bperan/src/main/java/es/project/bperan/vo/UsuarioVO.java
+++ bperan/src/main/java/es/project/bperan/vo/UsuarioVO.java
@@ -4,11 +4,37 @@
 package es.project.bperan.vo;
 
 import java.io.Serializable;
+import java.util.Date;
 
-/**
- * @author fredo
- *
- */
 public class UsuarioVO implements Serializable {
 
+	private int idusuario;
+	
+	private String nombre;
+	
+	public int getIdusuario() {
+		return idusuario;
+	}
+
+	public void setIdusuario(int idusuario) {
+		this.idusuario = idusuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	private Date fechaCreacion;
 }
