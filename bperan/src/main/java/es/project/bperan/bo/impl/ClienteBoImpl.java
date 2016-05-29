3510e579f5a7255964d330f0bd5eea49fcb6fab1
carosufe <carosufe@gmail.com>
2016-05-29 18:40:13 +0200

    Solucionado el delete de cliente con presupuestos asociados

--- bperan/src/main/java/es/project/bperan/bo/impl/ClienteBoImpl.java
+++ bperan/src/main/java/es/project/bperan/bo/impl/ClienteBoImpl.java
@@ -1,25 +1,32 @@
 package es.project.bperan.bo.impl;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.springframework.transaction.annotation.Transactional;
 
 import es.project.bperan.bo.GenericBO;
 import es.project.bperan.dao.GenericDAO;
 import es.project.bperan.pojo.Cliente;
+import es.project.bperan.pojo.Presupuesto;
 
 
 @Transactional
 public class ClienteBoImpl implements GenericBO<Cliente> {
 
 	private GenericDAO<Cliente> clienteDAO;
-	
+	private GenericDAO<Presupuesto> presupuestoDAO;
+		
 	public ClienteBoImpl() {
 		super();
 	}
 	
 	public void setClienteDAO(GenericDAO<Cliente> clienteDAO) {
 		this.clienteDAO = clienteDAO;
+	}
+	
+	public void setPresupuestoDAO(GenericDAO<Presupuesto> presupuestoDAO) {
+		this.presupuestoDAO = presupuestoDAO;
 	}
 
 	@Override
@@ -37,6 +44,14 @@ public class ClienteBoImpl implements GenericBO<Cliente> {
 	@Override
 	public void delete(Cliente pojo) {
 		Cliente cliente = clienteDAO.findById(pojo.getIdcliente()); 
+		
+		Presupuesto presupuesto = new Presupuesto();
+		presupuesto.setCliente(cliente);
+		Collection<Presupuesto> listaPresupuestoCliente = presupuestoDAO.findByPojo(presupuesto); 	
+		Iterator<Presupuesto> itPresupuesto = listaPresupuestoCliente.iterator();		 
+		while (itPresupuesto.hasNext()) {		 
+			presupuestoDAO.delete(itPresupuesto.next());			
+		}
 		
 		clienteDAO.delete(cliente);
 		
